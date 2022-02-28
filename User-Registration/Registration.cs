ed3742c2f549225fe33117b97ae4badb7e989828
Sampada <sampadahakke102@gmail.com>
2022-02-28 22:34:28 +0530

    [Sampada] Adding Validation for last name

--- User-Registration/Registration.cs
+++ User-Registration/Registration.cs
@@ -15,14 +15,25 @@ namespace User_Registration
             Console.Write("Enter the name = ");
             string name = Console.ReadLine();
             string Pattern = "^[A-Z][a-zA-Z]{2,}$";
-
             if (Validate(name,Pattern))
                 Console.WriteLine("The entered name is valid!!");
             else
                 Console.WriteLine("Please enter the valid name..");
         }
 
-       //Creating method to check validation
+        //Creating method to check validation for last name
+        public void LastName()
+        {
+            Console.WriteLine("Enter the last name = ");
+            string name = Console.ReadLine();
+            string pattern = "^[A-Z][a-zA-Z]{2,}$";
+            if (Validate(name, pattern))
+                Console.WriteLine("The entered last name is valid!!");
+            else
+                Console.WriteLine("Please enter the valid last name..");
+        }
+
+        //Creating method to check validation
         public bool Validate(string info, string Pattern)
         {
            return Regex.IsMatch( info, Pattern);
