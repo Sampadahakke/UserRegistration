34cbdc4f2f80de7d2317b335cea08fc5ec031e15
Sampada <sampadahakke102@gmail.com>
2022-02-28 22:28:22 +0530

    [Sampada] Adding Validation for first name

--- User-Registration/Program.cs
+++ User-Registration/Program.cs
@@ -11,45 +11,9 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("==========Welcome To User Registration===========");
+            //Creating object od class
             Registration registration = new Registration();
-            bool flag = true;
-
-            while (flag)
-            {
-                Console.WriteLine("\nEnter the user info :  \n1.FirstName  \n2.LastName  \n3.Email Id  \n4.MobileNumber \n5.Exit");
-                int choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        registration.FirstName();
-                        break;
-                    case 2:
-                        registration.LastName();
-                        break;
-                    case 3:
-                        registration.EmailId();
-                        break;
-                    case 4:
-                        registration.MobileNumber();
-                        break;
-                    case 5:
-                        Console.WriteLine("Do you want to exit = Y/N");
-                        char option = Convert.ToChar(Console.ReadLine());
-                        if(option == 'Y')
-                        {
-                            flag = false;
-                            Console.WriteLine("Exited");
-                        }
-                        else
-                        {
-                            flag = true;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice");
-                        break;
-                }
-            }
+            registration.FirstName();
             Console.ReadKey();
         }
 
--- User-Registration/Registration.cs
+++ User-Registration/Registration.cs
@@ -12,57 +12,18 @@ namespace User_Registration
         //Creating method to check validation for first name
         public void FirstName()
         {
-            Console.WriteLine("Enter the name = ");
+            Console.Write("Enter the name = ");
             string name = Console.ReadLine();
             string Pattern = "^[A-Z][a-zA-Z]{2,}$";
 
-            if (ValidateName(name,Pattern))
+            if (Validate(name,Pattern))
                 Console.WriteLine("The entered name is valid!!");
             else
                 Console.WriteLine("Please enter the valid name..");
         }
 
-        //Creating method to check validation for last name
-        public void LastName()
-        {
-            Console.WriteLine("Enter the last name = ");
-            string name = Console.ReadLine();
-            string pattern = "^[A-Z][a-zA-Z]{2,}$";
-
-            if (ValidateName(name, pattern))
-                Console.WriteLine("The entered last name is valid!!");
-            else
-                Console.WriteLine("Please enter the valid last name..");
-        }
-
-        //Creating method to check validation for email id
-        public void EmailId()
-        {
-            Console.WriteLine("Enter the email id = ");
-            string emailId = Console.ReadLine();
-            string pattern = "^[A-Za-z0-9]{3,}([.][A-Za-z0-9]{3,})?[@][a-zA-Z]{2,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2})?$";
-
-            if (ValidateName(emailId, pattern))
-                Console.WriteLine("The entered email id is valid!!");
-            else
-                Console.WriteLine("Please enter the valid email id..");
-        }
-
-        //Creating method to check validation for mobile number
-        public void MobileNumber()
-        {
-            Console.WriteLine("Enter the mobile number = ");
-            string mobileNumber = Console.ReadLine();
-            string pattern = "^([0-9]{2}[ ]){0,1}[0-9]{10}$";
-
-             if (ValidateName(mobileNumber, pattern))
-                Console.WriteLine("The entered mobile number is valid!!");
-            else
-                Console.WriteLine("Please enter the valid mobile number..");
-        }
-
-        //Creating method to check validation
-        public bool ValidateName(string info, string Pattern)
+       //Creating method to check validation
+        public bool Validate(string info, string Pattern)
         {
            return Regex.IsMatch( info, Pattern);
         }
