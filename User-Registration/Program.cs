323185e63b0b2effce3896281f3471d6add431a7
Sampada <sampadahakke102@gmail.com>
2022-02-28 21:21:40 +0530

    [Sampada] Adding Validation for last name

--- User-Registration/Program.cs
+++ User-Registration/Program.cs
@@ -11,16 +11,40 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("==========Welcome To User Registration===========");
-
-            //Creating object of class
             Registration registration = new Registration();
-            registration.UserInfo();
-            if(registration.ValidateName())
+            bool flag = true;
+
+            while (flag)
             {
-                Console.WriteLine("The entered name is valid");
+                Console.WriteLine("\nEnter the user info :  \n1.FirstName  \n2.LastName \n3.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        registration.FirstName();
+                        break;
+                    case 2:
+                        registration.LastName();
+                        break;
+                    case 3:
+                        Console.WriteLine("Do you want to exit = Y/N");
+                        char option = Convert.ToChar(Console.ReadLine());
+                        if(option == 'Y')
+                        {
+                            flag = false;
+                            Console.WriteLine("Exited");
+                        }
+                        else
+                        {
+                            flag = true;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+
+                }
             }
-            else
-                Console.WriteLine("Please enter the valid name");
             Console.ReadKey();
         }
 
