11ae67465e92147c3e41688867faa979ecbab2d9
Sampada <sampadahakke102@gmail.com>
2022-02-28 19:29:00 +0530

    [Sampada] Adding Validation for name

--- User-Registration/Program.cs
+++ User-Registration/Program.cs
@@ -11,7 +11,18 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("==========Welcome To User Registration===========");
+
+            //Creating object of class
+            Registration registration = new Registration();
+            registration.UserInfo();
+            if(registration.ValidateName())
+            {
+                Console.WriteLine("The entered name is valid");
+            }
+            else
+                Console.WriteLine("Please enter the valid name");
             Console.ReadKey();
         }
+
     }
 }
--- User-Registration/Registration.cs
+++ User-Registration/Registration.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace User_Registration
 {
     internal class Registration
     {
+        //Initializing Variables
+        public static string namePattern = "^[A-Z][a-zA-Z]{2,}$";
+        string Name;  
+        
+        //Creating method to take user info
+        public void UserInfo()
+        {
+            Console.Write("Enter the name = ");
+            this.Name = Console.ReadLine();
+        }
+
+        //Creating method to check validation
+        public bool ValidateName()
+        {
+           return Regex.IsMatch(Name, namePattern);
+        }
+
     }
 }
